630b75049e46a0cb284f63c5a1dbb35b78bc534a
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-04-10 01:29:01 +0530

    Add files via upload

---
+++ code24.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL)
+            head = newNode;
+        else
+            temp->next = newNode;
+
+        temp = newNode;
+    }
+    return head;
+}
+
+struct Node* deleteKey(struct Node* head, int key) {
+    struct Node *temp = head, *prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return head;
+
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, key;
+
+    scanf("%d", &n);
+    struct Node* head = createList(n);
+
+    scanf("%d", &key);
+
+    head = deleteKey(head, key);
+
+    printList(head);
+
+    return 0;
+}
