f8c6f537b070e6c10db361de804afd64e9f55fb2
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-04-10 01:28:06 +0530

    Add files via upload

---
+++ code22.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL)
+            head = newNode;
+        else
+            temp->next = newNode;
+
+        temp = newNode;
+    }
+
+    int count = 0;
+    temp = head;
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);  /
+        count++;
+        temp = temp->next;
+    }
+
+    printf("\nCount = %d", count);
+
+    return 0;
+}
