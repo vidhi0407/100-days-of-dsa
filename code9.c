c03a452447e730991192144d28f24f778f3156c9
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-04-09 22:57:48 +0530

    Add files via upload

---
+++ code9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    scanf("%s", str);
+
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        char temp = str[left];
+        str[left] = str[right];
+        str[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
