4e33448adb5e0ad6852fbf0b23fe5682698b0681
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-04-13 14:01:22 +0530

    Add files via upload

---
+++ code58.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+int search(int inorder[], int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct Node* buildTree(int preorder[], int inorder[], int start, int end, int* preIndex) {
+    if (start > end)
+        return NULL;
+
+    struct Node* root = createNode(preorder[*preIndex]);
+    (*preIndex)++;
+
+    if (start == end)
+        return root;
+
+    int inIndex = search(inorder, start, end, root->data);
+
+    root->left = buildTree(preorder, inorder, start, inIndex - 1, preIndex);
+    root->right = buildTree(preorder, inorder, inIndex + 1, end, preIndex);
+
+    return root;
+}
+
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int preorder[100], inorder[100];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &preorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    int preIndex = 0;
+
+    struct Node* root = buildTree(preorder, inorder, 0, n - 1, &preIndex);
+
+    postorder(root);
+
+    return 0;
+}
