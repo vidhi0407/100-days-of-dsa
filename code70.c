9616af985eda1358fe3345f9b38b20218c51f9aa
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-04-13 14:05:38 +0530

    Add files via upload

---
+++ code70.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 100
+
+struct Edge {
+    int u, v, w;
+};
+
+int main() {
+    int n, m;
+    scanf("%d", &n); 
+    scanf("%d", &m); 
+
+    struct Edge edges[MAX];
+
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
+    }
+
+    int src;
+    scanf("%d", &src);
+
+    int dist[MAX];
+
+    for (int i = 0; i < n; i++)
+        dist[i] = INT_MAX;
+
+    dist[src] = 0;
+
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = 0; j < m; j++) {
+            int u = edges[j].u;
+            int v = edges[j].v;
+            int w = edges[j].w;
+
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+
+    for (int j = 0; j < m; j++) {
+        int u = edges[j].u;
+        int v = edges[j].v;
+        int w = edges[j].w;
+
+        if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+            printf("NEGATIVE CYCLE");
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d -> %d = %d\n", src, i, dist[i]);
+    }
+
+    return 0;
+}
