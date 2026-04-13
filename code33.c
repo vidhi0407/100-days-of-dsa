44085beebee89026357722fc7aabf71a9a104404
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-04-13 13:52:59 +0530

    Add files via upload

---
+++ code33.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char x) {
+    stack[++top] = x;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i = 0, j = 0;
+
+    scanf("%s", infix);
+
+    while (infix[i] != '\0') {
+        char ch = infix[i];
+
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop();
+        }
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(ch)) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+        i++;
+    }
+
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}
