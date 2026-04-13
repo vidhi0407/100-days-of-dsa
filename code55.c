5120755618f5808604de2275f1a91d4478c5e1fa
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-04-13 14:00:29 +0530

    Add files via upload

---
+++ code55.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* root = createNode(arr[0]);
+    struct Node* queue[100];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void rightView(struct Node* root) {
+    if (root == NULL) return;
+
+    struct Node* queue[100];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+            struct Node* curr = queue[front++];
+
+            if (i == size - 1)
+                printf("%d ", curr->data);
+
+            if (curr->left)
+                queue[rear++] = curr->left;
+
+            if (curr->right)
+                queue[rear++] = curr->right;
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+
+    rightView(root);
+
+    return 0;
+}
