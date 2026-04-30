a26dc965042b2bd680b95a19f0f85f4674875bbc
vidhi0407 <vidhi.goyal210407@gmail.com>
2026-05-01 01:22:55 +0530

    Add files via upload

---
+++ code94.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void countingSort(int arr[], int n) {
+    int max = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    int *count = (int *)calloc(max + 1, sizeof(int));
+
+    for(int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for(int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    int output[n];
+    for(int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+
+    free(count);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    countingSort(arr, n);
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
